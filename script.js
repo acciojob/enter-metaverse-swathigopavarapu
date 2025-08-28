4ef64f435a095aa8ed8fadd0d9b923d0fb0cdd4a
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-28 12:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-  // get elements
-  const statusPara = document.getElementById("status");
-  const enterBtn = document.getElementById("enterBtn");
-
-  // add click event
-  enterBtn.addEventListener("click", function () {
-    statusPara.innerHTML = "<h1>Entered Metaverse</h1>";
-  });
-});
