fec660d1ea81bd6d983713f3164a7ac528550c2e
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-28 12:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function () {
+  // grab the <p> tag
+  const status = document.getElementById("status");
+
+  // change its innerHTML to an <h1>
+  status.innerHTML = "<h1>Entered Metaverse</h1>";
+});
